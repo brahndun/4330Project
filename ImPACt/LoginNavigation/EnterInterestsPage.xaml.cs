d957771e8baadc2affec2ebacdf2bef8b2e6d22b
LindsayPrescott <46791323+LindsayPrescott@users.noreply.github.com>
2019-12-04 17:07:05 -0600

    Update EnterInterestsPage.xaml.cs

--- ImPACt/LoginNavigation/EnterInterestsPage.xaml.cs
+++ ImPACt/LoginNavigation/EnterInterestsPage.xaml.cs
@@ -13,6 +13,8 @@ namespace LoginNavigation
             InitializeComponent();
         }
 
+        //When the enter key is pressed after typing in an interest, this function adds the typed interest to the user's list of interests
+        //Then uploads it to the displayed list of interests.
         public void EnterPressed(object sender, EventArgs e)
         {
             //Users can only have 10 interests at maximum.
@@ -32,6 +34,8 @@ namespace LoginNavigation
             }
         }
 
+        //When next is cliked, the added interests is added to the current user's list of interests on the database and the app proceeds
+        //to the next screen.
         public async void OnNextButtonClicked(object sender, EventArgs e)
         {
             App.UserLoggedIn.Interests = String.Join("^", interests.ToArray());
